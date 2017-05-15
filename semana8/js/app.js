85a2c14e158721bacd2e8e6b62714ce8c9dbdb13
Vitor Neves <vitorneves@gmail.com>
2017-05-15 02:57:15 -0300

    Resposta da semana 8

---
+++ semana8/js/app.js
@@ -0,0 +1,68 @@
+(function(){
+'use strict';
+
+var shoppingList2 = [
+  {
+    name: "Milk",
+    quantity: "2"
+  },
+  {
+    name: "Donuts",
+    quantity: "200"
+  },
+  {
+    name: "Cookies",
+    quantity: "300"
+  },
+  {
+    name: "Chocolate",
+    quantity: "5"
+  }
+];
+
+angular.module('ShoppingListCheckOff',[])
+.controller('ToBuyController',ToBuyController)
+.controller('AlreadyBoughtController',AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+
+function ShoppingListCheckOffService(){
+    var service = this;
+    console.log('service:',service);
+    var toBuyList = shoppingList2;
+    var boughtList = [];
+
+    service.getBuyItems = function(){
+        return toBuyList;
+    };
+
+    service.getBoughtList = function(){
+        return boughtList;
+    };
+
+    service.buyItem = function(itemIndex){
+        console.log('buying item: ', itemIndex);
+        boughtList.push(toBuyList[itemIndex]);
+        toBuyList.splice(itemIndex, 1);
+    };
+};
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService){
+    var addItem = this;
+    console.log('addItem',addItem);
+    addItem.items = ShoppingListCheckOffService.getBuyItems();
+
+    addItem.buyItem = function (itemIndex){
+        ShoppingListCheckOffService.buyItem(itemIndex);
+    }
+};
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController (ShoppingListCheckOffService){
+    var showList = this;
+    console.log('showList',showList);
+    showList.items = ShoppingListCheckOffService.getBoughtList();
+};
+
+})();
