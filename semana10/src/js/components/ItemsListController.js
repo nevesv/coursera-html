0c60c7973a9885d9d95659debfa3bd2048784de3
nevesv <vitorneves@gmail.com>
2017-06-12 12:57:41 -0300

    Create ItemsListController.js

--- semana10/src/js/components/ItemsListController.js
+++ semana10/src/js/components/ItemsListController.js
@@ -4,8 +4,8 @@
 angular.module('MenuApp')
 .controller('ItemsListController',ItemsListController);
 
-ItemsListController.$Inject = ['MenuDataService', 'menuItems'];
-function ItemsListController (MenuDataService, menuItems){
+ItemsListController.$Inject = ['menuItems'];
+function ItemsListController (menuItems){
 	var itemsList = this;
 	itemsList.menuItems = menuItems;
 }
